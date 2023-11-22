e9738111d8b6c0cc48935ebd906981967bb383b5
Duke-Skygawker <bart3kk990@gmail.com>
2023-11-22 02:24:40 +0100

    custom logo spin&scale

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const Greeting = () => {
+  return (
+    <>
+      <div className="scaler">
+        <img className="img-spin" src="logo512.png" alt="" />
+      </div>
+      <h2>My First Component</h2>
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Greeting />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
