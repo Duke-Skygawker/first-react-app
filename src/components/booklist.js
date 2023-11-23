ed928409a37ae59a6c8eb40eaf4057a06c10aba2
Duke-Skygawker <bart3kk990@gmail.com>
2023-11-23 02:35:15 +0100

    basic event examples

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -20,18 +20,36 @@ const books = [
     title: "The Ballad of Songbirds and Snakes",
   },
 ];
+
+const EventExamples = () => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+  const handleButtonClick = () => {
+    alert("handle button click");
+  };
+  return (
+    <section style={{ margin: "2rem" }}>
+      <form>
+        <h2>Basic Form</h2>
+        <input
+          type="text"
+          name="example"
+          onChange={handleFormInput}
+          style={{ margin: "2rem" }}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click me</button>
+    </section>
+  );
+};
+
 const Booklist = () => {
   return (
     <section className="booklist">
+      <EventExamples />
       {books.map((book) => {
-        return (
-          <Book
-            imgSrc={book.imgSrc}
-            title={book.title}
-            author={book.author}
-            key={book.title}
-          />
-        );
+        return <Book {...book} key={book.title} />;
       })}
     </section>
   );
