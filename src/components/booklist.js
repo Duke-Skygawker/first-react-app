aba2d9080e56aad5d4f7bb0cc0b21e8bec131796
Duke-Skygawker <bart3kk990@gmail.com>
2023-11-23 04:07:34 +0100

    list heading added

--- src/components/booklist.js
+++ src/components/booklist.js
@@ -3,12 +3,15 @@ import { books } from "./books.js";
 
 const Booklist = () => {
   return (
-    <section className="booklist">
-      {books.map((book, index) => {
-        const bookIndex = index + 1;
-        return <Book {...book} key={book.title} bookIndex={bookIndex} />;
-      })}
-    </section>
+    <>
+      <h1 className="booklist-heading">Amazon's Best Sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          const bookIndex = index + 1;
+          return <Book {...book} key={book.title} bookIndex={bookIndex} />;
+        })}
+      </section>
+    </>
   );
 };
 export default Booklist;
